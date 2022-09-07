abd31e341f8d7a90803024cc0f56d7cff0bf5e48
Jasper Rutte <jasperrutte@gmail.com>
2022-09-07 18:48:27 +0200

    Toegevoegd opdrachten, fruitmand, pannekoeken, hello en spiekbriefje.

---
+++ spiekbriefje.py
@@ -0,0 +1,31 @@
+###################################
+# commentaar
+###################################
+# start commentaar met een hashtag
+# dit is commentaar en kan ook achter de code
+from re import MULTILINE
+
+
+x = 3 # dit is commentaar
+
+###################################
+# datatypes
+
+#str zijn woorden en zinnen
+woord = "woord"
+zin = "dit is een zin"
+#int zijn hele getallen zonder komma's
+y = 4
+#float zijn getallen met een komma
+z = 4.90930293
+
+#print print letterlijk iets naar de console
+#str print een tekst  naar de console
+#multlines zijn om meerdere lines aan str te maken in een keer
+"""
+dit zit in de string
+en dit ook
+en dit ook 
+en dit ook
+"""
+
