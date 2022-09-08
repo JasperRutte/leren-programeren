3de4037f9ecf167b0ac465451f4abaaf285fb783
Jasper Rutte <jasperrutte@gmail.com>
2022-09-08 10:54:24 +0200

    speelhal opdracht

---
+++ projects/Module-02/feestlunch.py
@@ -0,0 +1,5 @@
+croissant = 0.39 * 17
+stokbrood = 2.78 * 2
+kortingsbon = 0.50 * 3
+totaal = croissant + stokbrood - kortingsbon
+print(totaal)
---
+++ projects/Module-02/speelhal.py
@@ -0,0 +1,4 @@
+toegangticket = 7.45 * 3
+vipkaart = 0.37 * 9 * 3
+totaal = toegangticket + vipkaart
+print(totaal)
