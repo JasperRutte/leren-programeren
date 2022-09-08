65b8752f3dd04ffcdc97e8a8760b03f617df655c
Jasper Rutte <jasperrutte@gmail.com>
2022-09-08 12:11:45 +0200

    Update feestlunch en speelhal

--- projects/Module-02/feestlunch.py
+++ projects/Module-02/feestlunch.py
@@ -4,6 +4,6 @@ hoeveel_kortingsbonnen = int(input("Hoeveel kortingsbonnen heeft u? "))
 
 croissant = 0.39 * hoeveel_crossiants
 stokbrood = 2.78 * hoeveel_stokbroden
-kortingsbon = 0.50 * hoeveel_kortingsbonnen
+kortingsbon = 1.50 * hoeveel_kortingsbonnen
 totaal = croissant + stokbrood - kortingsbon
-print(f'De feestlunch kost je bij de bakker {totaal} euro voor de {hoeveel_crossiants} croissantjes en de {hoeveel_stokbroden} stokbroden als de {hoeveel_kortingsbonnen} kortingsbonnen nog geldig zijn!')
+print(f'De feestlunch kost je bij de bakker {totaal:.2f} euro voor de {hoeveel_crossiants} croissantjes en de {hoeveel_stokbroden} stokbroden als de {hoeveel_kortingsbonnen} kortingsbonnen nog geldig zijn!')
