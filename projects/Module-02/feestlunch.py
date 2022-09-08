4731cc000156fb83e834a9f93805c920de637f07
Jasper Rutte <jasperrutte@gmail.com>
2022-09-08 11:03:27 +0200

    Opdrachten feestlunch en speelhal update

--- projects/Module-02/feestlunch.py
+++ projects/Module-02/feestlunch.py
@@ -2,4 +2,4 @@ croissant = 0.39 * 17
 stokbrood = 2.78 * 2
 kortingsbon = 0.50 * 3
 totaal = croissant + stokbrood - kortingsbon
-print(totaal)
+print(f'De feestlunch kost je bij de bakker {totaal} euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!')
--- projects/Module-02/speelhal.py
+++ projects/Module-02/speelhal.py
@@ -1,4 +1,4 @@
-toegangticket = 7.45 * 3
-vipkaart = 0.37 * 9 * 3
+toegangticket = 7.45 * 4
+vipkaart = 0.37 * 9 * 4
 totaal = toegangticket + vipkaart
-print(totaal)
+print(f'Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar {totaal} euro')
