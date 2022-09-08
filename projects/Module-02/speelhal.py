df02cccae1169e44de090596bad0b82b6aefa3e3
Jasper Rutte <jasperrutte@gmail.com>
2022-09-08 12:12:09 +0200

    update feestlunch en speelhal

--- projects/Module-02/speelhal.py
+++ projects/Module-02/speelhal.py
@@ -1,7 +1,7 @@
-aantal_personen = float(input("Hoeveel personen zijn er? "))
-aantal_minuten = float(input("Hoeveel minuten wilt u VR spelen? "))
+aantal_personen = int(input("Hoeveel personen zijn er? "))
+aantal_minuten = int(input("Hoeveel minuten wilt u VR spelen? "))
 
 toegangticket = 7.45 * aantal_personen
 vipkaart = 0.37 * aantal_minuten * aantal_personen
 totaal = toegangticket + vipkaart
-print(f'Dit geweldige dagje-uit met {aantal_personen} mensen in de Speelhal met {aantal_minuten} minuten VR kost je maar {totaal} euro')
+print(f'Dit geweldige dagje-uit met {aantal_personen} mensen in de Speelhal met {aantal_minuten} minuten VR kost je maar {totaal:.2f} euro')
