0ae2ef24847965cedbf6c987c5daa1ec5bc73981
Jasper Rutte <jasperrutte@gmail.com>
2022-09-22 10:30:45 +0200

    opdracht smartphone.py

--- projects/chalanges/smartphone.py
+++ projects/chalanges/smartphone.py
@@ -1,18 +1,14 @@
 iphone = float(input("Hoeveel kost de iphone? "))
 samsung = float(input("Hoeveel kost de Samsung? "))
-asus = float(input("Hoeveel kost de Asus: "))
+asus = float(input("Hoeveel kost de Asus? "))
 prijsVerschil = iphone - samsung
-verschilAsusIphone = asus -
-verschilAsusSamsung =
 
-if asus <
-
-elif iphone >= 900 and 900 <= samsung < iphone:
+if iphone >= 900 and samsung >= 900 and iphone > samsung:
     print(f"De Iphone is het duurst, de telefoon kost: {iphone} euro")
     print(f"De Samsung is het goedkoopst, de telefoon kost: {samsung} euro")
     print("Het advies is om geen telefoon te kopen, ze zijn te duur.")
 
-elif 900 <= iphone < samsung and samsung >= 900:
+elif iphone >= 900 and samsung >= 900 and iphone < samsung:
     print(f"De Samsung is het duurst, de telefoon kost: {samsung} euro")
     print(f"De Iphone is het goedkoopst, de telefoon kost: {iphone} euro")
     print("Het advies is om geen telefoon te kopen, ze zijn te duur.")
