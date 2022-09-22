402e6fc6ac63bebfbd6b6b267206c79e9aab47ee
Jasper Rutte <jasperrutte@gmail.com>
2022-09-22 10:13:47 +0200

    opdracht update

--- projects/chalanges/smartphone.py
+++ projects/chalanges/smartphone.py
@@ -2,24 +2,34 @@ iphone = float(input("Hoeveel kost de iphone? "))
 samsung = float(input("Hoeveel kost de Samsung? "))
 prijsVerschil = iphone - samsung
 
-if prijsVerschil <= 50 and iphone > samsung:
-    print(f"De Iphone is het duurst, de telefoon kost: {iphone}")
-    print(f"De Samsung is het goedkoopst, de telefoon kost: {samsung}")
+if iphone >= 900 and samsung >= 900 and iphone > samsung:
+    print(f"De Iphone is het duurst, de telefoon kost: {iphone} euro")
+    print(f"De Samsung is het goedkoopst, de telefoon kost: {samsung} euro")
+    print("Het advies is om geen telefoon te kopen, ze zijn te duur.")
+
+elif iphone >= 900 and samsung >= 900 and iphone < samsung:
+    print(f"De Samsung is het duurst, de telefoon kost: {samsung} euro")
+    print(f"De Iphone is het goedkoopst, de telefoon kost: {iphone} euro")
+    print("Het advies is om geen telefoon te kopen, ze zijn te duur.")
+
+elif prijsVerschil <= 50 and iphone > samsung:
+    print(f"De Iphone is het duurst, de telefoon kost: {iphone} euro")
+    print(f"De Samsung is het goedkoopst, de telefoon kost: {samsung} euro")
     print(f"Het advies is dus de Iphone te kopen. Deze is namelijk {prijsVerschil} euro duurder/goedkoper dan de Iphone")
 
 elif prijsVerschil <= 50 and iphone < samsung:
-    print(f"De Samsung is het duurst, de telefoon kost: {samsung}.")
-    print(f"De Iphone is het goedkoopst, de telefoon kost: {iphone}.")
+    print(f"De Samsung is het duurst, de telefoon kost: {samsung} euro.")
+    print(f"De Iphone is het goedkoopst, de telefoon kost: {iphone} euro.")
     print(f"Het advies is dus de Iphone te kopen. Deze is namelijk {prijsVerschil} euro duurder/goedkoper dan de Iphone.")
 
 elif iphone > samsung:
-    print(f"De Iphone is het duurst, de telefoon kost: {iphone}.")
-    print(f"De Samsung is het goedkoopst, de telefoon kost: {samsung}.")
+    print(f"De Iphone is het duurst, de telefoon kost: {iphone} euro.")
+    print(f"De Samsung is het goedkoopst, de telefoon kost: {samsung} euro.")
     print(f"Het advies is dus de samsung te kopen. Deze is namelijk {prijsVerschil} euro duurder/goedkoper dan de Iphone.")
 
 elif iphone < samsung:
-    print(f"De Samsung is het duurst, de telefoon kost: {samsung}.")
-    print(f"De Iphone is het goedkoopst, de telefoon kost: {iphone}.")
+    print(f"De Samsung is het duurst, de telefoon kost: {samsung} euro.")
+    print(f"De Iphone is het goedkoopst, de telefoon kost: {iphone} euro.")
     print(f"Het advies is dus de Iphone te kopen. Deze is namelijk {prijsVerschil} euro duurder/goedkoper dan de Iphone.")
 
 else:
