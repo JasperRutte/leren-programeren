1547105da75f1a0b811e3beb57e5a4f9cbc3a6cd
Jasper Rutte <jasperrutte@gmail.com>
2022-10-12 11:10:28 +0200

    opdracht robotarm.py

--- projects/Leren-programmeren/Module-03/coin.py
+++ projects/Leren-programmeren/Module-03/coin.py
@@ -1,24 +1,23 @@
-# name of student:
-# number of student:
-# purpose of program:
-# function of program:
+# name of student: Jasper Rutte
+# number of student: 99071902
+# purpose of program: To see how much change to give and how much change you gave
+# function of program: calculate how much change to give, how much change has been given and how much change you should've payed
 # structure of program:
 
-toPay = int(float(input('Amount to pay: ')) * 100)  #
-paid = int(float(input('Paid amount: ')) * 100)  #
-change = paid - toPay  #
+toPay = int(float(input('Amount to pay: ')) * 100)  # toPay is a user input * 100
+paid = int(float(input('Paid amount: ')) * 100)  # paid is a user input * 100
+change = paid - toPay  # change is var paid - var toPay
 amountReturned = ""
 payed = [500, 300, 200, 50, 20, 10, 5, 2, 1]
 payedString = ""
 
-if change > 0:  #
-    coinValue = 500  #
+if change > 0:  # check if var change is greater than 0
+    coinValue = 500  # giving coinValue a value
 
-    while change > 0 and coinValue > 0:  #
-        nrCoins = change // coinValue  #
-
-        if nrCoins > 0:  #
-            print('return maximal ', nrCoins, ' coins of ', coinValue, ' cents!')  #
+    while change > 0 and coinValue > 0:  # check if change is greater than 0 and coinValue is greater than 0
+        nrCoins = change // coinValue  # var nrCoins is change divided by coinValue with extra's
+        if nrCoins > 0:  # check if nrCoins is greater than 0
+            print('return maximal ', nrCoins, ' coins of ', coinValue, ' cents!')  # print a string
             nrCoinsReturned = int(input('How many coins of ' + str(coinValue) + ' cents did you return? '))  #
             change -= nrCoinsReturned * coinValue  #
             amountReturned += str(coinValue) + " x" + str(nrCoinsReturned) + "\n"
