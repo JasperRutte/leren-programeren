0d4fadf79bdd6743eb0bb5e97ca008dfeaed4964
Jasper Rutte <jasperrutte@gmail.com>
2022-10-14 09:25:42 +0200

    challange zwembad.py

--- projects/challenges/zwembaden.py
+++ projects/challenges/zwembaden.py
@@ -1,1 +1,16 @@
-print(8 * 3 * 1.5)
+x = float(input("Lengte: "))
+y = float(input("Breedte: "))
+z = float(input("Hoogte/Diepte: "))
+kubiekeMeter = x * y * z
+
+afstand = int(input("Afstand van het bedrijf: "))
+
+uitgave = kubiekeMeter * 25
+afvoerGrond = 32.5 * kubiekeMeter
+totaal = afvoerGrond + uitgave
+
+print(f"""
+Offerte voor een zwembad van {x} bij {y} bij {z} meter (inhoud:{kubiekeMeter}m3)
+Uitgaven: {uitgave}
+Afvoeren grond {afvoerGrond}
+Totaal: {totaal}""")
