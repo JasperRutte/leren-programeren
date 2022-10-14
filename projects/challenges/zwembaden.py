cd7bf6ab6fb00d3efe2cb0045552eead71eb06f2
Jasper Rutte <jasperrutte@gmail.com>
2022-10-14 09:41:43 +0200

    challange zwembad.py

--- projects/challenges/zwembaden.py
+++ projects/challenges/zwembaden.py
@@ -1,16 +1,28 @@
 x = float(input("Lengte: "))
 y = float(input("Breedte: "))
 z = float(input("Hoogte/Diepte: "))
+afstand = int(input("Afstand van het bedrijf (km): "))
+
 kubiekeMeter = x * y * z
-
-afstand = int(input("Afstand van het bedrijf: "))
-
-uitgave = kubiekeMeter * 25
+uitgraven = kubiekeMeter * 25
 afvoerGrond = 32.5 * kubiekeMeter
-totaal = afvoerGrond + uitgave
+
+if afstand < 50 and kubiekeMeter < 20:
+    voorrijkosten = afstand * 1.25
+    totaal = uitgraven + 100 + voorrijkosten
+elif afstand >= 50 and kubiekeMeter < 20:
+    voorrijkosten = afstand * 1.15
+    totaal = uitgraven + 100 + voorrijkosten
+elif afstand < 50 and kubiekeMeter >= 20:
+    voorrijkosten = afstand * 2.15
+    totaal = uitgraven + 250 + voorrijkosten
+else:
+    voorrijkosten = afstand * 2.05
+    totaal = uitgraven + 250 + voorrijkosten
 
 print(f"""
 Offerte voor een zwembad van {x} bij {y} bij {z} meter (inhoud:{kubiekeMeter}m3)
-Uitgaven: {uitgave}
-Afvoeren grond {afvoerGrond}
+Uitgaven: {uitgraven}
+Afvoeren grond: {afvoerGrond}
+voorrijkosten {voorrijkosten}
 Totaal: {totaal}""")
