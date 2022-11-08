d663c1ed2161a5f84cd423cf10b06e3c05216659
Jasper Rutte <jasperrutte@gmail.com>
2022-11-08 09:45:40 +0100

    opdracht fruitmand.py

--- projects/Leren-programmeren/Module-04/test.py
+++ projects/Leren-programmeren/Module-04/test.py
@@ -1,4 +1,24 @@
 from fruitmand import fruitmand
+import random
+
+# opdracht 1
 # print(fruitmand)
 
-print(len(fruitmand))
+# opdracht 2
+# print(len(fruitmand))
+
+# opdracht 3
+# for i in range(0,len(fruitmand)):
+#     print(fruitmand[i]['name'])
+
+# opdracht 4
+# user_input = int(input("Hoeveel: "))
+# random_fruit = random.randint(0, 6)
+#
+# for i in range(0, user_input):
+#     print(fruitmand[random_fruit]['name'])
+
+# opdracht 5
+# a = len(fruitmand) - 1
+# for i in range((a), -1, -1):
+#     print(fruitmand[i]['name'])
