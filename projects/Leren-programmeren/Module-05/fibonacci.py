623d260c44484705dc2a9ff9d0bfbb2dac13a0b1
Jasper Rutte <jasperrutte@gmail.com>
2022-12-21 09:16:33 +0100

    update gitignore

--- projects/Leren-programmeren/Module-05/fibonacci.py
+++ projects/Leren-programmeren/Module-05/fibonacci.py
@@ -4,7 +4,7 @@ def fibo(nummer: int):
     oud2 = 0
     reeks = []
     test = []
-    for x in range(0, 100):
+    for x in range(0, 50):
         reeks.append(oud)
         oud = nieuw
         nieuw += oud2
@@ -15,8 +15,32 @@ def fibo(nummer: int):
     elif nummer in reeks:
         for x in range(0, len(reeks)):
             if reeks[x] == nummer:
-                for y in range(x-1, len(reeks)):
+                for y in range(x, x + 10):
                     test.append(reeks[y])
     return test
 
-print(fibo(55))
+print(fibo(3))
+
+
+def fibo2(nummer: int):
+    nieuw = 1
+    oud = 0
+    oud2 = 0
+    reeks = []
+    test = []
+    for x in range(0, 50):
+        reeks.append(oud)
+        oud = nieuw
+        nieuw += oud2
+        oud2 = oud
+    if nummer not in reeks:
+        print(f"{nummer} zit niet in de reeks...")
+    else:
+        for x in reeks:
+            if x == nummer:
+                test.append(x)
+                return test
+            else:
+                test.append(x)
+
+print(fibo2(1))
