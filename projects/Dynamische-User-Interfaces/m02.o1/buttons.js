84929f203ac7278c370daf3104c0ba7fc2ce45e4
Jasper Rutte <jasperrutte@gmail.com>
2023-03-14 16:35:11 +0100

    buttons 2 af

--- projects/Dynamische-User-Interfaces/m02.o1/buttons.js
+++ projects/Dynamische-User-Interfaces/m02.o1/buttons.js
@@ -1,6 +1,7 @@
 let counters = [0, 0, 0]
 
 buttonCountUp = function(buttonId){
+
     counters[Number(buttonId.id - 1)] += 1
     document.getElementById(buttonId.id).innerHTML = counters[buttonId.id -  1]
     document.getElementById("img").src = "./images/" + String(buttonId.id) + ".jpg";
