c43d9c064f7695d9696b3e445dee905103f8c7a2
Jasper Rutte <jasperrutte@gmail.com>
2023-03-23 09:16:32 +0100

    to-do list

--- projects/Dynamische-User-Interfaces/m02.o4/script.js
+++ projects/Dynamische-User-Interfaces/m02.o4/script.js
@@ -1,26 +1,19 @@
-// for (let int = 0; int < 6; int++){
-//     let int2 = int
-//     int2 = document.createElement("p")
-//     console.log(int2)
-//     int2.innerHTML = "\n"
-//     for (let numb = 0; numb < 9; numb++){
-//         // let numb2 = numb
-//         numb2 = document.createElement("button");
-//         numb2.innerHTML = "Hello Button";
-//         numb2.setAttribute("id", "-" + String(numb))
-//         document.body.appendChild(numb2);
-//     }
-// }
+function changeColor(btn) {
+    let btnColor = btn.style.backgroundColor
+    if (btnColor === "rgb(0, 255, 0)"){
+        return
+    }
 
-//
-// const n = document.createElement("button");
-// n.setAttribute("id", "button" + String(n))
+
+
+}
 
 let container = document.getElementById('container');
 for (let i = 1; i < 30 + 1; i++) {
     let button = document.createElement("button");
     button.innerHTML = i;
-    button.setAttribute('id', i);
-    button.style.backgroundColor = '#00cc00';
+    button.setAttribute("id", i);
+    button.setAttribute("onClick", "changeColor(this)");
+    button.style.backgroundColor = "#00ff00";
     container.appendChild(button);
 }
