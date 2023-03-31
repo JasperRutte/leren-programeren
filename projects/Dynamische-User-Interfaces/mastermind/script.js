3696f4564d12d0c939c91044861177402589f470
Jasper Rutte <jasperrutte@gmail.com>
2023-03-31 09:31:03 +0200

    mastermind

--- projects/Dynamische-User-Interfaces/mastermind/script.js
+++ projects/Dynamische-User-Interfaces/mastermind/script.js
@@ -1,12 +1,30 @@
-let container = document.getElementById('container');
+colors = ["", "green", "blue", "red", "purple"]
 
-for (let i = 1; i < 48 + 1; i++) {
-    let button = document.createElement("select");
-    container.appendChild(button);
+function submit(select){
+    let number = select.id
+    console.log(select.id)
+    document.getElementById(select.id).disabled = true;
+    for (let i = 1; i < 4 + 1; i++) {
+        document.getElementById(String(number)+"-"+String(i)).disabled = true
+    }
 }
-//
-// function myFunction() {
-//     var x = document.getElementById("mySelect");
-//     var option = document.createElement("option");
-//     option.text = "Kiwi";
-//     x.add(option);
+
+for (let collumn = 1; collumn < 12 + 1; collumn++) {
+    for (let row = 1; row < 4 + 1; row++) {
+        let select = document.createElement("select");
+        let selectId = collumn+"-"+row
+        select.setAttribute("id", selectId);
+        for (let numb2 = 0; numb2 < colors.length; numb2++) {
+            let option = document.createElement("option");
+            option.value = colors[numb2];
+            option.text = colors[numb2];
+            select.appendChild(option);
+            container.appendChild(select)
+        }
+    }
+    let submit = document.createElement("button");
+    submit.setAttribute("id", collumn);
+    submit.setAttribute("onClick", "submit(this)")
+    submit.innerHTML = "SUBMIT"
+    container.appendChild(submit)
+}
