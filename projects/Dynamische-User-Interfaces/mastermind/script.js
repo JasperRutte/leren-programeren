8684286933bbcd8bda502cd19bd3295e8d066a5c
Jasper Rutte <jasperrutte@gmail.com>
2023-04-05 17:05:08 +0200

    mastermind assignment

--- projects/Dynamische-User-Interfaces/mastermind/script.js
+++ projects/Dynamische-User-Interfaces/mastermind/script.js
@@ -1,29 +1,52 @@
-colors = ["", "green", "blue", "red", "purple"]
+const rows = 4
+const collumn = 12
+const colors = ["", "green", "blue", "red", "purple", "yellow", "orange"]
+
+const randomColors = []
+console.log(colors.length - 2)
+for (let i = 1; i < rows + 1; i++){
+    let randomNumber = Math.floor(Math.random() * (colors.length - 1)) + 1;
+    randomColors.push(colors[randomNumber])
+}
+
+console.log(randomColors)
+
 
 function submit(select){
     let number = select.id
-    console.log(select.id)
+    console.log(number)
     document.getElementById(select.id).disabled = true;
-    for (let i = 1; i < 4 + 1; i++) {
+    let rowColors = []
+    for (let i = 1; i < rows + 1; i++) {
+        let selectId = document.getElementById(String(number+"-"+i))
         document.getElementById(String(number)+"-"+String(i)).disabled = true
+        rowColors.push(selectId.value)
+        console.log(rowColors)
+    }
+    for (let test = 0; test < randomColors.length; test++){
+        if (rowColors[test] === randomColors[test]){
+            console.log("correct")
+        } else {
+            console.log("wrong")
+        }
     }
 }
 
-for (let collumn = 1; collumn < 12 + 1; collumn++) {
-    for (let row = 1; row < 4 + 1; row++) {
+
+for (let num1 = 1; num1 < collumn + 1; num1++) {
+    for (let num2 = 1; num2 < rows + 1; num2++) {
         let select = document.createElement("select");
-        let selectId = collumn+"-"+row
-        select.setAttribute("id", selectId);
-        for (let numb2 = 0; numb2 < colors.length; numb2++) {
+        select.setAttribute("id", num1+"-"+num2);
+        for (let color = 0; color < colors.length; color++) {
             let option = document.createElement("option");
-            option.value = colors[numb2];
-            option.text = colors[numb2];
+            option.value = colors[color];
+            option.text = colors[color];
             select.appendChild(option);
             container.appendChild(select)
         }
     }
     let submit = document.createElement("button");
-    submit.setAttribute("id", collumn);
+    submit.setAttribute("id", num1);
     submit.setAttribute("onClick", "submit(this)")
     submit.innerHTML = "SUBMIT"
     container.appendChild(submit)
---
+++ projects/challenges/button/script.js
@@ -0,0 +1,7 @@
+let container = document.getElementById('container');
+
+let button = document.createElement("button");
+button.innerHTML = "Test";
+button.setAttribute("id", "test1");
+container.appendChild(button);
+
